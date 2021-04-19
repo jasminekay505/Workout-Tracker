33329a39060d13b53beb0548833fa26a00c5568f
Jasmine Kay <jasminekay505@gmail.com>
2021-04-19 11:19:45 -0600

    Fixing spacing in html.js

--- routes/html.js
+++ routes/html.js
@@ -2,17 +2,17 @@ const router = require("express").Router();
 const path = require('path');
 
 //Home page 
-router.get('/', (req,res) => { 
+router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 //Exercise page
-router.get('/exercise', (req,res) => { 
+router.get('/exercise', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/exercise.html'));
 });
 
 //Stats page
-router.get('/stats', (req,res) => { 
+router.get('/stats', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/stats.html'));
 });
 
